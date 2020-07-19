bd0c7f43f0bc39580486b6e01e40a0dd3e70118a
JustAlejandro <32211773+JustAlejandro@users.noreply.github.com>
2020-07-18 23:57:39 -0500

    Revert doesn't reset blending

--- BLMGJ/src/OffScreenRender.h
+++ BLMGJ/src/OffScreenRender.h
@@ -79,5 +79,7 @@ protected:
 		glEnable(GL_DEPTH_TEST);
 		glDepthFunc(GL_ALWAYS);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	}
 };
